2b2570f89f9ad39551996c5ac0e9d49ba62550f9
AnaBeatriz686 <estvs.ab@edu.unifil.br>
2026-04-11 17:56:35 -0300

    feat: implementa listagem de jogos (GET /api/jogos)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-let filmes = [
+let jogos = [
     { id: 1, nome: "Resident Evil: Requiem", preco: 160, categoria: "Terror", ano: 2026},
     { id: 2, nome: "Hades", preco: 74, categoria: "RPG", ano: 2020},
     { id: 3, nome: "The Last of Us Part I", preco: 250, categoria: "Ação", ano: 2023},
@@ -15,6 +15,11 @@ let filmes = [
     { id: 9, nome: "Red Dead Redemption 2", preco: 300, categoria: "Ação", ano: 2019},
     { id: 10, nome: "The Farmer Was Replaced", preco: 30, categoria: "Simulação", ano: 2025}
 ];
+let proximoId = 11;
+
+app.get('/api/jogos', (req, res) => {
+    res.json(jogos);
+});
 
 app.listen(3000, () => {
     console.log('API CRUD completa na porta 3000');
